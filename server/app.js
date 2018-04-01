c0a341d6c1e8c13888a33ee482a9589a63e27e1b
benjipelletier <pelletierbenji@gmail.com>
2018-04-01 02:04:30 -0400

    node/firebase setup

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
+const firebase = require('firebase');
+const config = require('./keys.json');
 const request = require('request');
+
+
+firebase.initializeApp(config)
+var database = firebase.database();
+database.ref('/tracks/').once('value').then((data) => {
+    console.log("Data: " + data.val());
+    firebase.database().goOffline();
+});
+
+
