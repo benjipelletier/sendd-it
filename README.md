5ce5ce397937d11d3cb23a8cb95e9b177e18c397
benjipelletier <pelletierbenji@gmail.com>
2018-04-02 21:30:02 -0400

    Express setup

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ A no-signup drop & go audio sharing solution for instant playback and feedback, 
  <div align="center">
     <img src="assets/sendd-it-demo.png" width="100%" alt="demo" title="demo">
 </div>
-
--- server/app.js
+++ server/app.js
@@ -1,20 +1,28 @@
 const firebase = require('firebase')
+const express = require('express')
+
 const config = require('./keys.json')
-const request = require('request')
+
+const app = express();
 
 firebase.initializeApp(config)
 const firebaseApp = firebase.app()
 const database = firebase.database()
 
-firebaseApp.auth().signInAnonymously().then(() => {
-    database.ref('/comments/').once('value').then((data) => {
-        console.log("Data: " + JSON.stringify(data.val()));
-    }).catch()
+var courses = [1, 2, 3]
 
-    // database.ref('/comments').push({ //returns id
-    //     author: "gracfehop",
-    //     title: "Announcing COBOL, a New Programming Language"
-    //   });
-}).catch(function (error) {
-    console.log(error.code + ': ' + error.message)
-});
+app.get('/test:id', (req, res) => {
+    res.send(courses)
+})
+
+// firebaseApp.auth().signInAnonymously().then(() => {
+//     // database.ref('/comments').push({ //returns id
+//     //     author: "gracfehop",
+//     //     title: "Announcing COBOL, a New Programming Language"
+//     //   });
+// }).catch(function (error) {
+//     console.log(error.code + ': ' + error.message)
+// });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`))
